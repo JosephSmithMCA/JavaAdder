7e96b1b2ddefb9469b5de699832ac43432b64128
JosephSmithMCA <152203982+JosephSmithMCA@users.noreply.github.com>
2024-04-19 11:48:19 -0500

    JavaAdder

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+﻿var buttonElement = document.getElementById("addbutton");
+buttonElement.addEventListener("click", btnClicked);
+
+function btnClicked() {
+    //grab elements
+    var output = document.getElementById("sum");
+    var number1 = document.getElementById("num1");
+    var number2 = document.getElementById("num2");
+
+    //grab values
+    var nums1 = parseFloat(number1.value);
+    var nums2 = parseFloat(number2.value);
+
+    //calculate result
+    var result = nums1 + nums2;
+    
+    //display value
+    output.value = result;
+}
